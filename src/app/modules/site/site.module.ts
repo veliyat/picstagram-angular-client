33a3a598b61b52d8c2c963a5d5adef79781c14b6
Ravi Veliyat <veliyat@gmail.com>
2019-07-21 23:03:27 +0530

    Layout of the home page without login done

--- src/app/modules/site/site.module.ts
+++ src/app/modules/site/site.module.ts
@@ -5,11 +5,19 @@ import { SiteRoutingModule } from './site-routing.module'
 
 import { SiteComponent } from './site.component';
 import { SiteLayoutComponent } from './site-layout/site-layout.component';
+import { SiteHeaderComponent } from './site-layout/site-header/site-header.component';
+import { SiteFooterComponent } from './site-layout/site-footer/site-footer.component';
+import { SiteNavComponent } from './site-layout/site-nav/site-nav.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     SiteLayoutComponent,
-    SiteComponent
+    SiteComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent,
+    SiteNavComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
